294844c7a728f2a129250e09bc1742b8a9af6135
Alamin <codepapa360@gmail.com>
2023-05-11 01:22:43 +0600

    Cleaned global variables

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,49 @@
 "use strict";
 import "../sass/main.scss";
 
-const root = document.documentElement;
-const form = document.querySelector(".form");
-const totalAmountOutput = document.getElementById("total-amount");
-const pageViewOutput = document.getElementById("page-view-text");
-const slider = document.getElementById("price-range");
-const durationCheckbox = document.getElementById("duration-checkbox");
-const durationText = document.getElementById("duration-text");
-const views = ["10k", "50k", "100k", "500k", "1M"];
-const allCosts = [4, 8, 16, 24, 36];
+(function () {
+  const root = document.documentElement;
+  const form = document.querySelector(".form");
+  const totalAmountOutput = document.getElementById("total-amount");
+  const pageViewOutput = document.getElementById("page-view-text");
+  const slider = document.getElementById("price-range");
+  const durationCheckbox = document.getElementById("duration-checkbox");
+  const durationText = document.getElementById("duration-text");
+  const views = ["10k", "50k", "100k", "500k", "1M"];
+  const allCosts = [4, 8, 16, 24, 36];
 
-const renderOutputs = function () {
-  const inputedRange = +slider.value;
-  const yearly = durationCheckbox.checked;
-  let totalCost = allCosts[inputedRange];
+  const renderOutputs = function () {
+    const inputedRange = +slider.value;
+    const yearly = durationCheckbox.checked;
+    let totalCost = allCosts[inputedRange];
 
-  //Slider filled up value chnaging
-  const value = ((inputedRange - slider.min) / (slider.max - slider.min)) * 100;
-  root.style.setProperty("--slider-value", value + "%");
+    //Slider filled up value chnaging
+    const value =
+      ((inputedRange - slider.min) / (slider.max - slider.min)) * 100;
+    root.style.setProperty("--slider-value", value + "%");
 
-  if (yearly) {
-    const months = totalCost * 12;
-    const discounted = months - (months * 25) / 100;
-    totalCost = discounted;
-  }
+    if (yearly) {
+      const months = totalCost * 12;
+      const discounted = months - (months * 25) / 100;
+      totalCost = discounted;
+    }
 
-  durationText.textContent = yearly ? "year" : "month";
-  totalAmountOutput.textContent = `$${totalCost}.00`;
-  pageViewOutput.textContent = views[inputedRange];
-  return;
-};
-renderOutputs();
-
-slider.addEventListener("input", function () {
+    durationText.textContent = yearly ? "year" : "month";
+    totalAmountOutput.textContent = `$${totalCost}.00`;
+    pageViewOutput.textContent = views[inputedRange];
+    return;
+  };
   renderOutputs();
-});
 
-durationCheckbox.addEventListener("change", function () {
-  renderOutputs();
-});
+  slider.addEventListener("input", function () {
+    renderOutputs();
+  });
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-});
+  durationCheckbox.addEventListener("change", function () {
+    renderOutputs();
+  });
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
+})();
